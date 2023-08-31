ebb13e48149b56d7235c56a567b356cffc8d43a1
BrandonERivera <brandonellis.rivera@gmail.com>
2023-08-31 13:01:40 -0400

    added to coloring and style, have local storage set up need to show now

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var Currentcity = document.querySelector(".currentday-data")
 var searchbtnel = document.querySelector(".search");
+var forecast = document.querySelector(".futureday-data");
+var h4 = document.querySelector("h4")
 var apiKey = "cf54dde4a7df3e380b59cabd8dfa3ea7"
 var baseURL = "https://api.openweathermap.org/data/2.5/forecast?";
 var storedhistory = [];
@@ -11,86 +13,93 @@ function search(){
 
     fetch(link)
     .then(function (response) {
-      console.log(response)
       return response.json();
     })
     .then(function (data){
-      console.log(data)
-        var searchName = data.city.name;
-        var searchLat = data.city.coord.lat;
-        var searchLon = data.city.coord.lon;
-        var searchicon = data.list[0].weather[0].description;
-        var weatherDate = data.list[0].dt_txt.split(" ")[0];
-        var datesplit = weatherDate.split("-")
-        var dateformat = datesplit[1] + "/" + datesplit[2] + "/" + datesplit[0];
-        var searchTemp = data.list[0].main.temp + " °F";
-        var searchWind = data.list[0].wind.speed + " MPH";
-        var searchHumid = data.list[0].main.humidity + "%"
-        var citydata = {
-          name: searchName,
-          date: dateformat,
-          icon: searchicon,
-          temp: searchTemp,
-          wind: searchWind,
-          humid: searchHumid
-        }
-
-        showcurrentweather(citydata)
-        Storelink(searchName, link)
+        showcurrentweather(data)
+        getForecast(data)
+        Storelink(data, link)
         Currentcity.style.display ="block"
 
 
 
     })
 }
-function getForcast(lat , lon){
-  var link = baseURL + "lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
-  fetch(link)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data){
-    console.log(data)
+function getForecast(data){
+  forecast.innerHTML= "";
+  h4.style.display = "block"
+  for(var i = 1; i < data.list.length; i+=8){
+    var date = dayjs.unix(data.list[i].dt).format('MM/DD/YYYY')
+    var icon = data.list[i].weather[0].icon;
+    var iconImg = `https://openweathermap.org/img/wn/${icon}@2x.png`
+    var temp = data.list[i].main.temp + " °F";
+    var wind = data.list[i].wind.speed + " MPH";
+    var humid = data.list[i].main.humidity + "%"
+    var card = document.createElement("div");
+    card.setAttribute("class", "card m-2 p-3 w-25 bg-info bg-gradient")
+    var dateEL = document.createElement("p");
+    var iconEL = document.createElement("img");
+    var tempEL = document.createElement("p");
+    var windEL = document.createElement("p");
+    var humidEL = document.createElement("p");
+  
+    dateEL.textContent = date;
+    iconEL.src = iconImg;
+    tempEL.textContent = "Temperature: "+temp;
+    windEL.textContent = "Wind Speed: "+wind;
+    humidEL.textContent = "Humidity: " +humid;
+    card.append(dateEL, iconEL, tempEL, windEL, humidEL);
+    forecast.append(card);
+  
 
+    
 
+  }
 
-  })
 
 }
+ 
+
+
 function showcurrentweather(data){
   Currentcity.innerHTML= "";
-  var name = data.name
-  var date = data.date
-  var icon = data.icon
-  var temp = data.temp
-  var wind = data.wind
-  var humid = data.humid
+  var name = data.city.name;
+  var date = dayjs.unix(data.list[0].dt).format('MM/DD/YYYY')
+  var icon = data.list[0].weather[0].icon;
+  var iconImg = `https://openweathermap.org/img/wn/${icon}@2x.png`
+  var temp = data.list[0].main.temp + " °F";
+  var wind = data.list[0].wind.speed + " MPH";
+  var humid = data.list[0].main.humidity + "%"
   var nameEL = document.createElement("h3");
   var dateEL = document.createElement("p");
-  var iconEL = document.createElement("p");
+  var iconEL = document.createElement("img");
   var tempEL = document.createElement("p");
   var windEL = document.createElement("p");
   var humidEL = document.createElement("p");
 
-  nameEL.textContent = "City: "+name;
-  dateEL.textContent = "Date: " +date;
-  iconEL.textContent = icon;
-  tempEL.textContent = "Temperature: "+temp;
-  windEL.textContent = "Wind Speed: "+wind;
-  humidEL.textContent = "Humidity: " +humid;
-  console.log(nameEL)
+  nameEL.textContent = "City: "+ name;
+  dateEL.textContent = "Date: " + date;
+  iconEL.src = iconImg;
+  tempEL.textContent = "Temperature: "+ temp;
+  windEL.textContent = "Wind Speed: "+ wind;
+  humidEL.textContent = "Humidity: " + humid;
   Currentcity.append(nameEL, dateEL, iconEL, tempEL, windEL, humidEL);
 
 
 }
-function Storelink(data, data2){
-  var storedhistory = JSON.parse(localStorage.getItem("storedhistory"))
+ function Storelink(data, link){
+  var name = data.city.name
+  console.log(name + link)
+  var storedhistory = JSON.parse(localStorage.getItem("storedhistory")) || []
   var history = {
-    name: data,
-    link: data2
+    name: name,
+    link: link
   }
+  console.log(storedhistory)
   console.log(history)
-  storedhistory.push(history);
+
+  storedhistory.push(history)
+
   localStorage.setItem("storedhistory", JSON.stringify(storedhistory));
 
 
